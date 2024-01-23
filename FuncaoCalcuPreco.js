73096e2f1b6c79aed711ab57bc7dd33ba3a20295
karolinybispo <karol-bobio@hotmail.com>
2024-01-23 20:05:12 -0300

    precos das etiquetas em funcoes

---
+++ FuncaoCalcuPreco.js
@@ -0,0 +1,19 @@
+
+let produto = 123;
+
+function descon(valor){
+    let resul = produto * valor / 100;
+    return  resul = produto - resul;  
+}
+
+function duasOuMais(vezes){
+    if(vezes < 2){
+    let resulta = produto / vezes;
+            }
+            else {
+                let resulta = produto * 10 /100;
+                resulta = produto + resulta;
+                let acresci = resulta / vezes;
+                console.log(acresci);
+            }
+}
---
+++ verfiMaiorIdade.js
@@ -0,0 +1,13 @@
+//funcao para ver se sou maior de idade
+
+function verIdade(idade){
+    if(idade >= 18){
+       console.log("maior idade "+ idade);
+    }
+    else{
+        console.log("menor idade ", idade);
+       
+    }
+}
+
+verIdade(0);
