8ebe45d293b4fa93bfddc90310707a5f82a9a03b
karolinybispo <karol-bobio@hotmail.com>
2024-01-23 22:03:17 -0300

    segun commit calc preco cm funcao

--- FuncaoCalcuPreco.js
+++ FuncaoCalcuPreco.js
@@ -1,19 +1,17 @@
-
-let produto = 123;
-
-function descon(valor){
-    let resul = produto * valor / 100;
-    return  resul = produto - resul;  
+function descon(descon, produto){
+    let resul = produto - (produto * descon / 100);
+          return resul;
 }
 
-function duasOuMais(vezes){
-    if(vezes < 2){
-    let resulta = produto / vezes;
-            }
+function duasOuMais(vezes, produto){
+    if(vezes === 2){
+        let resulta = produto / vezes;
+            console.log(resulta);
+                    }
             else {
-                let resulta = produto * 10 /100;
-                resulta = produto + resulta;
-                let acresci = resulta / vezes;
-                console.log(acresci);
-            }
+                let resulta = (produto + (produto * 10 /100)) / 3;
+                     console.log(resulta);
+                }
 }
+console.log(descon(10, 200));
+duasOuMais(3,150);
